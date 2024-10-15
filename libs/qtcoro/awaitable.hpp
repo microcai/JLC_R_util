54ac40074e8d7435e39e66302ca7e936cc2978ff
microcai <microcaicai@gmail.com>
2024-10-15 12:08:38 +0800

    终极 awaitable_detached

--- libs/qtcoro/awaitable.hpp
+++ libs/qtcoro/awaitable.hpp
@@ -4,64 +4,37 @@
 #include <QTimer>
 #include <QtCore>
 #include <coroutine>
+#include <utility>
+
+	struct debug_coro_promise
+	{
+
+		void* operator new(std::size_t size)
+		{
+			void* ptr = std::malloc(size);
+			if (!ptr)
+			{
+				throw std::bad_alloc{};
+			}
+			qDebug() << "promise " << ptr << " created";
+			return ptr;
+		}
+
+		void operator delete(void* ptr, [[maybe_unused]] std::size_t size)
+		{
+			qDebug() << "promise " << ptr << " freeed";
+			std::free(ptr);
+		}
+
+	};
+
 
 namespace qtcoro
 {
-	// 协程包装...
-	struct awaitable_detached
+	template <typename T>
+	struct promise_type_base
 	{
-		struct promise_type
-		{
-			awaitable_detached get_return_object()
-			{
-				return awaitable_detached{ };
-			}
-
-			std::suspend_never initial_suspend() noexcept { return {}; }
-			std::suspend_never final_suspend() noexcept { return {}; }
-
-			void unhandled_exception() { }
-
-			void return_void() { }
-		};
-	};
-
-	template <typename T>
-	struct awaitable;
-
-	template <typename T>
-	struct awaitable_promise;
-
-	template <typename T>
-	struct FinalAwaiter
-	{
-		bool await_ready() noexcept { return false; }
-		void await_resume() noexcept { }
-		std::coroutine_handle<> await_suspend(std::coroutine_handle<awaitable_promise<T>> h) noexcept
-		{
-			if (h.promise().continuation)
-			{
-				return h.promise().continuation;
-			}
-			return std::noop_coroutine();
-		}
-	};
-
-	// Promise 类型实现...
-	template <typename T>
-	struct awaitable_promise
-	{
-		std::coroutine_handle<> continuation;
 		T value; // 用于存储协程返回的值
-
-		awaitable<T> get_return_object();
-
-		void reset_handle(std::coroutine_handle<> h) { continuation = h; }
-
-		auto initial_suspend() { return std::suspend_always{}; }
-
-		auto final_suspend() noexcept { return FinalAwaiter<T>{}; }
-		void unhandled_exception() { }
 
 		template <typename V>
 		void return_value(V&& v) noexcept
@@ -71,29 +44,170 @@ namespace qtcoro
 	};
 
 	template <>
-	struct awaitable_promise<void>
+	struct promise_type_base<void>
 	{
-		std::coroutine_handle<> continuation;
-		awaitable<void> get_return_object();
-
-		void reset_handle(std::coroutine_handle<> h) { continuation = h; }
-
-		auto initial_suspend() { return std::suspend_always{}; }
-
-		auto final_suspend() noexcept { return FinalAwaiter<void>{}; }
-
-		void unhandled_exception() { }
-
-		void return_void() { }
+		constexpr void return_void() noexcept { }
 	};
+
+	// 协程包装...
+	template <typename T = void>
+	struct awaitable_detached
+	{
+
+		awaitable_detached(const awaitable_detached&) = delete;
+
+		struct promise_type : public promise_type_base<T>, public debug_coro_promise
+		{
+			awaitable_detached get_return_object()
+			{
+				return awaitable_detached{this};
+			}
+
+			struct joiner_job
+			{
+				constexpr bool await_ready() const noexcept { return false; }
+
+				std::coroutine_handle<> await_suspend(std::coroutine_handle<>) const noexcept
+				{
+					std::coroutine_handle<void> promise_handle = std::coroutine_handle<promise_type>::from_promise(*this_);
+
+					if (this_->no_owner)
+					{
+						promise_handle.destroy();
+					}
+					if (this_->joiner)
+						return this_->joiner;
+					return std::noop_coroutine();
+				}
+
+				constexpr void await_resume() const noexcept {}
+
+				promise_type* this_;
+			};
+
+			auto initial_suspend() noexcept { return std::suspend_never{}; }
+			auto final_suspend() noexcept { return joiner_job{this}; }
+
+			void unhandled_exception() { }
+
+			std::coroutine_handle<> joiner;
+			bool no_owner = false;
+		};
+
+		awaitable_detached(promise_type* promise) : promise(promise) {}
+		awaitable_detached(awaitable_detached && other)
+			: promise(other.promise)
+		{
+			other.promise = nullptr;
+		}
+
+		~awaitable_detached()
+		{
+			if (promise)
+			{
+				std::coroutine_handle<void> promise_handle = std::coroutine_handle<promise_type>::from_promise(*promise);
+
+				if (promise_handle.done())
+				{
+					promise_handle.destroy();
+				}
+				else
+				{
+					promise->no_owner = true;
+				}
+			}
+		}
+
+		bool await_ready() noexcept { return false ;}
+
+		auto await_suspend(std::coroutine_handle<> continuation) noexcept
+		{
+			promise->joiner = continuation;
+			return std::noop_coroutine();
+		}
+
+		auto await_resume() noexcept
+		{
+
+		}
+
+		promise_type* promise;
+	};
+
+	template <typename T>
+	struct awaitable;
+
+	template <typename T>
+	struct awaitable_promise;
+
+	// Promise 类型实现...
 
 	// awaitable 协程包装...
 	template <typename T>
 	struct awaitable
 	{
-		using promise_type = awaitable_promise<T>;
+		struct promise_type : public promise_type_base<T>, public debug_coro_promise
+		{
+			std::coroutine_handle<> continuation;
 
-		awaitable(std::coroutine_handle<promise_type> h)
+			awaitable get_return_object() { return awaitable { std::coroutine_handle<promise_type>::from_promise(*this) } ; }
+
+			void reset_handle(std::coroutine_handle<> h) { continuation = h; }
+
+			auto initial_suspend() { return std::suspend_always{}; }
+
+			auto final_suspend() noexcept {
+
+				struct FinalAwaiter
+				{
+					bool await_ready() noexcept { return false; }
+					void await_resume() noexcept { }
+					std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> h) noexcept
+					{
+						if (h.promise().continuation)
+						{
+							return h.promise().continuation;
+						}
+						return std::noop_coroutine();
+					}
+				};
+
+				return FinalAwaiter{};
+			}
+			void unhandled_exception() { }
+
+			template<typename AnyType>
+			auto await_transform(awaitable<AnyType> && awaiter)
+			{
+				return std::move(awaiter);
+			}
+
+			template<typename CallbackFunc> requires ( std::is_invocable_r<void, CallbackFunc, std::coroutine_handle<>>::value )
+			auto await_transform(CallbackFunc&& awaitee)
+			{
+				struct awaiter
+				{
+					CallbackFunc awaitee;
+
+					constexpr bool await_ready() noexcept { return false; }
+					constexpr void await_resume() noexcept {}
+					void await_suspend(std::coroutine_handle<> handle) noexcept
+					{
+						awaitee( handle );
+					}
+				};
+
+				return awaiter{awaitee};
+			}
+
+			template<typename AwaitableType>
+			auto await_transform(AwaitableType&& awaiter)
+			{
+				return std::forward<AwaitableType>(awaiter);
+			}
+		};
+
+		explicit awaitable(std::coroutine_handle<promise_type> h)
 			: current_coro_handle_(h)
 		{
 		}
@@ -128,20 +242,16 @@ namespace qtcoro
 		awaitable(const awaitable&)			   = delete;
 		awaitable& operator=(const awaitable&) = delete;
 
-		T operator()() { return get(); }
-
-		T get()
-		{
-			if constexpr (!std::is_same_v<T, void>)
-				return std::move(current_coro_handle_.promise().value);
-		}
-
-		bool await_ready() const noexcept { return false; }
+		bool await_ready() const noexcept { return current_coro_handle_ ? current_coro_handle_.done() : true; }
 
 		T await_resume()
 		{ // 修改返回类型为 T
 			if constexpr (!std::is_same_v<T, void>)
-				return std::move(current_coro_handle_.promise().value);
+			{
+				if (current_coro_handle_)
+					return std::move(current_coro_handle_.promise().value);
+				std::terminate();
+			}
 		}
 
 		auto await_suspend(std::coroutine_handle<> continuation)
@@ -150,95 +260,35 @@ namespace qtcoro
 			return current_coro_handle_;
 		}
 
-		std::coroutine_handle<promise_type> current_coro_handle_;
-
-		void detach()
+		auto detach()
 		{
-			auto launch_coro = [](awaitable<T> lazy) -> awaitable_detached {
-					co_await lazy;
+			auto launch_coro = [](awaitable<T> lazy) -> awaitable_detached<T> {
+					co_return co_await lazy;
 				};
 
-			[[maybe_unused]] auto detached = launch_coro(std::move(*this));
+			return launch_coro(std::move(*this));
 		}
+
+		std::coroutine_handle<promise_type> current_coro_handle_;
 	};
 
-	template <typename T>
-	inline awaitable<T> awaitable_promise<T>::get_return_object()
-	{
-		return awaitable<T>{ std::coroutine_handle<awaitable_promise<T>>::from_promise(*this) };
-	}
-
-	inline awaitable<void> awaitable_promise<void>::get_return_object()
-	{
-		return awaitable<void>{ std::coroutine_handle<awaitable_promise<void>>::from_promise(*this) };
-	}
-
 } // namespace qtcoro
-
-template <typename T, typename CallbackFunction>
-struct CallbackAwaiter
-{
-public:
-	CallbackAwaiter(CallbackFunction&& callback_function)
-		: callback_function_(std::move(callback_function))
-	{
-	}
-
-	bool await_ready() noexcept { return false; }
-
-	void await_suspend(std::coroutine_handle<> handle)
-	{
-		callback_function_(
-			[handle = std::move(handle), this](T t) mutable
-			{
-				result_ = std::move(t);
-				handle.resume();
-			});
-	}
-
-	T await_resume() noexcept { return std::move(result_); }
-
-private:
-	CallbackFunction callback_function_;
-	T result_;
-};
-
-template <typename CallbackFunction>
-struct CallbackAwaiter<void, CallbackFunction>
-{
-public:
-	CallbackAwaiter(CallbackFunction&& callback_function)
-		: callback_function_(std::move(callback_function))
-	{
-	}
-
-	bool await_ready() noexcept { return false; }
-
-	void await_suspend(std::coroutine_handle<> handle)
-	{
-		callback_function_([handle = std::move(handle)]() mutable { handle.resume(); });
-	}
-
-	void await_resume() noexcept { }
-
-private:
-	CallbackFunction callback_function_;
-};
-
-template <typename T, typename callback>
-CallbackAwaiter<T, callback> callback_awaitable(callback&& cb)
-{
-	return CallbackAwaiter<T, callback>{ std::forward<callback>(cb) };
-}
 
 template <typename INT>
 qtcoro::awaitable<int> coro_delay_ms(INT ms)
 {
-	co_await callback_awaitable<void>([ms](auto handle) { QTimer::singleShot(std::chrono::milliseconds(ms), handle); });
+	co_await [ms](auto continuation) {
+		qDebug() << "timer setup for " << continuation.address();
+
+		QTimer::singleShot(std::chrono::milliseconds(ms), [continuation](){
+		qDebug() << "timer fired on " << continuation.address();
+		continuation();
+	}); };
 	co_return ms;
 }
 
-inline void start_coro(qtcoro::awaitable<void>&& awaitable_coro)
+template <typename T>
+auto start_coro(qtcoro::awaitable<T>&& awaitable_coro)
 {
-	awaitable_coro.detach();
+	return awaitable_coro.detach();
 }
